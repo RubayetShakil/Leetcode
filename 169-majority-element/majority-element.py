eb972c4a97f4316719d717aaade9be06778e3571
RubayetShakil <ferdousrubayet4@gmail.com>
2025-02-04 01:09:36 +0600

    Time: 171 ms (7.10%) | Memory: 19.6 MB (16.47%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,15 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        if len(nums)<=1:
             return nums[0]
-
-        mid = len(nums) // 2
-        left_major = self.majorityElement(nums[:mid])
-        right_major = self.majorityElement(nums[mid:])
-
-        # Count occurrences of left_major and right_major in the full array
-        count = Counter(nums)
-
-        if count[left_major] > len(nums) // 2:
-            return left_major
-        return right_major
+        mid=len(nums)//2
+        left=self.majorityElement(nums[:mid])
+        right=self.majorityElement(nums[mid:])
+        count_left=count_right=0
+        for i in nums:
+            if i==left:
+                count_left+=1
+        for i in nums:
+            if i==right:
+                count_right+=1
+        if count_left>len(nums)//2:
+            return left
+        else:
+            return right
+        
