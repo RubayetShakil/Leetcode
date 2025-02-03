267f207399d1692bb374a9412c8e28e2aef128d9
RubayetShakil <ferdousrubayet4@gmail.com>
2025-02-04 01:35:34 +0600

    Time: 3 ms (82.70%) | Memory: 19.3 MB (43.68%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,19 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        if len(nums)<=1:
-            return nums[0]
-        mid=len(nums)//2
-        left=self.majorityElement(nums[:mid])
-        right=self.majorityElement(nums[mid:])
-        count_left=count_right=0
+        count=0
+        candidate=None
+
         for i in nums:
-            if i==left:
-                count_left+=1
-        for i in nums:
-            if i==right:
-                count_right+=1
-        if count_left>len(nums)//2:
-            return left
-        else:
-            return right
+            if count==0:
+                candidate=i
+            if candidate==i:
+                count+=1
+            else:
+                count-=1
+        return candidate
         
